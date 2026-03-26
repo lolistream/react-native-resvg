e4f8cfe7ee485c637dcc7254335380ec33a3808f
lolistream <126906125+lolistream@users.noreply.github.com>
2026-03-26 17:03:12 +0300

    fix: correct toolchain

--- src/NativeReactNativeResvg.ts
+++ src/NativeReactNativeResvg.ts
@@ -7,4 +7,4 @@ export interface Spec extends TurboModule {
   cleanupRustCrate(): boolean;
 }
 
-export default TurboModuleRegistry.getEnforcing<Spec>('Resvg');
+export default TurboModuleRegistry.getEnforcing<Spec>('ReactNativeResvg');
